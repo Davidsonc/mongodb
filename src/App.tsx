b83a223efab7e0db64742a516a7da795fc0825a5
Davidsonc <davidsonc@gmail.com>
2025-02-06 19:32:16 -0300

    atualiza layout do componente App e modifica título para 'Clientes'

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 function App() {
   return (
-    <>
-      <div>
-        <h1 className="text-3xl">MEU APP REACT</h1>
-      </div>
-    </>
+    <div className="w-full min-h-screen bg-gray-900 flex flex-col items-center justify-center px-4">
+      <main className="my-10 w-full bg-red-500 md:max-w-2x1">
+        <h1 className="text-4xl font-medium text-white">Clientes</h1>
+      </main>
+    </div>
   );
 }
 
